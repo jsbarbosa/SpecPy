ff6bbffd47b4af40642e010e707e4d86181b054a
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2016-07-28 23:31:19 -0500

    Add files via upload

--- build/Linux/SpecPySplash.py
+++ build/Linux/SpecPySplash.py
@@ -1,91 +1,106 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jul 27 03:19:22 2016
+Created on Thu Jul 28 19:36:14 2016
 
 @author: juan
 """
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-
-import sys, time
-import image_qr
-import itertools
-
-#def variablename(var):
-#    return [tpl[0] for tpl in itertools.filter(lambda x: var is x[1], globals().items())]
-
-SETTINGS_FILE = "settings.cpy"
-CAMARA = 0
-PATH = "Captured"
-STREAMWIDTH = 680
-STREAMHEIGHT = 480
-
-app = QApplication(sys.argv)
+from PyQt4.uic import loadUiType
+from PyQt4 import QtCore, QtGui
+import sys, image_qr
+ 
+app = QtGui.QApplication(sys.argv)
 # Create and display the splash screen
-splash_pix = QPixmap(':/SpecPy.png')
-splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+splash_pix = QtGui.QPixmap(':/SpecPy.png')
+splash = QtGui.QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
 splash.show()
-time.sleep(3)
 app.processEvents()
 
-from SpecPy import *
-        
-def settingsReader(settingsfile):
-    instructions = []
-    file = open(settingsfile, 'r')
-    lines = file.readlines()
-    for line in lines:
-        parted_line = line.rpartition('=')
-        command = parted_line[0].replace(' ', '')
-        value = parted_line[-1].replace(' ', '').replace('\n', '')
-        instructions.append((command, value))
-    file.close()
-    return instructions
+import SpecPy as specpy
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import (FigureCanvasQTAgg as FigureCanvas)
 
-def settingsApplier(instructions):
-    global CAMARA, PATH, STREAMWIDTH, STREAMHEIGHT
-    for instruction in instructions:
-        command, value = instruction
-        if command == "camara":
-            if value.isdigit():
-                CAMARA = int(value)
+Ui_MainWindow, QMainWindow = loadUiType('mainwindow.ui')
+CAMARA = 0
+STREAMHEIGHT = 100
+STREAMWIDTH = int(4/3*STREAMHEIGHT)#300
+
+PATH = "Captures"
+
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self, ):
+        super(Main, self).__init__()
+        self.setupUi(self)
+        self.posSlider.valueChanged.connect(self.posChange)
+        self.sizeSlider.valueChanged.connect(self.sizeChange)
+        icon = QtGui.QIcon(':/SpecPy.png')
+        self.setWindowIcon(icon)
+        
+        self.show()
+        
+    def videoStream(self):
+        self.cam = specpy.GuiWindow("Stream", CAMARA, STREAMWIDTH, STREAMHEIGHT, PATH, "canvas")#, self.MainWindow)
+        self.fig_num = 0
+    def qImage(self, width, height, bytesPerLine):
+        qImg = QtGui.QImage(self.cam.frame.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888)
+        pix = QtGui.QPixmap(qImg)
+        return pix
+        
+    def update(self):
+        i = 0
+        while self.fig_num != None:
+            height = self.CamaraStream.frameGeometry().height()
+            width = int(4/3*height)            
+            self.cam.changeHeight(height)
+            self.cam.changeWidth(width)
+            bytesPerLine = 3 * width
+            answer = self.cam.eachPhotogram()
+            if not answer:
+                break
+            if i == 0:
+                self.updatePlot()
+                i += 1
             else:
-                CAMARA = value
-            print("Camara set to: ", value)
-        elif command == "path":
-            PATH = value
-            print("Path set to:", value)
-        elif command == "streamheight":
-            STREAMHEIGHT = int(value)
-            print("Stream height set to:", value)
-        elif command == "streamwidth":
-            STREAMWIDTH = int(value)
-            print("Stream width set to:", value)
-
-def settingsHandler(settingsfile):
-    if os.path.exists(settingsfile):
-        instructions = settingsReader(settingsfile)
-    else:
-        file = open(settingsfile, "w")
-        file.write("camara =" + str(0)+"\n")
-        file.write("path = " + PATH + "\n")
-        file.write("streamheight = " + str(STREAMHEIGHT) + "\n")
-        file.write("streamwidth = " + str(STREAMWIDTH) + "\n")
-        file.close()
-        instructions = settingsReader(settingsfile)
-    settingsApplier(instructions)
+                self.removePlot()
+                self.updatePlot()
+            pix = self.qImage(width, height, bytesPerLine)
+            self.CamaraStream.setPixmap(pix)
+            
+    def keyPressEvent(self, event):
+        keyPressed = event.key() #& 0xff
+        if keyPressed == QtCore.Qt.Key_Escape: 
+            self.cam.closeInput()
+            self.close()
+        elif keyPressed == QtCore.Qt.Key_Enter - 1:
+            self.fig_num = self.cam.captureData(self.cam.analysisFrame, self.fig_num)
+        elif keyPressed == QtCore.Qt.Key_C:
+            self.cam.cleanData()
     
-settingsHandler(SETTINGS_FILE)
-
-if not os.path.exists(PATH):
-    os.makedirs(PATH)
-else:
-    temp = glob.glob(PATH+'/*.jpg')
-    for item in temp:
-        os.remove(item)
+    def updatePlot(self):
+        self.canvas = FigureCanvas(self.cam.figure)
+        self.figureFrame.addWidget(self.canvas)
+        self.canvas.draw()
         
-cam = GuiWindow("Stream", CAMARA, STREAMWIDTH, STREAMHEIGHT, PATH)
+    def removePlot(self):
+        self.figureFrame.removeWidget(self.canvas)
+        self.canvas.close()
+    
+    def posChange(self):
+        size = self.posSlider.value()
+        self.cam.posTrackBar(size)
+        
+    def sizeChange(self):
+        size = self.sizeSlider.value()
+        self.cam.rangeTrackBar(size)
+    
+#aw = ApplicationWindow()
+#aw.setWindowTitle("Try")
+#aw.show()
 splash.finish(None)
-cam.loop()
+#app.exec_() 
+app = QtGui.QApplication(sys.argv)
+main = Main()
+main.videoStream()    
+main.update()
 sys.exit()
+#app.exec_()
